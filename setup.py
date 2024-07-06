703575a5865da85861028733a3f38e80ccba480d
Yasin <148462854+YShokrollahi@users.noreply.github.com>
2024-07-06 13:50:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='transformer_package',
+    name='transformers_tf',
     version='0.1',
     packages=find_packages(),
     install_requires=[
